8784751285e4318b1611ea12649d4a493c09b9d3
abdul-aziz <tamimaziz2007@gmail.com>
2023-11-02 16:46:12 +0600

    check

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,5 +72,19 @@ $(document).ready(function () {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+      breakpoints: {  
+        '1199': {
+          slidesPerView: 3,
+          spaceBetween: 20,
+      },
+        '767': {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        '480': {
+          slidesPerView: 1,
+          spaceBetween: 20,},
+
+      },
   });
 });
